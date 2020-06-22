e29a706a99bf81d8c73860254ff391281a01bd0a
EnigmaBreaker <cs5160398@cse.iitd.ac.in>
2020-06-22 10:50:49 +0530

    Added time based state update

--- parser.sh
+++ parser.sh
@@ -5,7 +5,7 @@ config="./config.json"
 
 timestamp=$(jq -r '.timestamp' $config)
 line=$(jq -r '.line' $config)
-skip=$(jq -r '.skip' $config)
+skip=10
 
 startLine=1
 
@@ -13,7 +13,6 @@ headTime=$(head -n 1 $input | jq -r '."@timestamp"')
 
 if [ $timestamp != $headTime ]
 then
-	# echo inside
 	timestamp=$headTime
 	line=1
 	startLine=1
@@ -21,31 +20,32 @@ else
 	startLine=$line
 fi
 
-
+lastSysTime=$(date +%s)
 count=$line
 while IFS= read -r line
 do
 	count=$((count + 1))
-
-	if ! ((count % skip))
+	currSysTime=$(date +%s)
+	echo $lastSysTime $currSysTime $((currSysTime - lastSysTime)) $skip
+	if ((currSysTime - lastSysTime > skip))
 	then
+		lastSysTime=$currSysTime
 		newTime=$(head -n 1 $input | jq -r '."@timestamp"')
 		if [ $newTime != $headTime ]
 		then
 			headTime=$newTime
 			count=1
 		fi
-		echo '{}' | jq --arg timestamp $headTime --arg line $count --arg skip $skip '{timestamp : $timestamp, line : $line, skip : $skip}' > $config
+		echo '{}' | jq --arg timestamp $headTime --arg line $count --arg skip $skip '{timestamp : $timestamp, line : $line}' > $config
 	fi
 
 	logtype=$(echo $line | jq .type)
-	if [ $logtype == "\"response\"" ]
+	if [[ $logtype == "\"response\"" ]]
 	then
-
 		url=$(echo $line | jq .req.url | cut -d"?" -f 1 | cut -d"\"" -f 2)
 		query=$(echo $line | jq .req.url | cut -d"?" -f 2 | cut -d"\"" -f 1)
 
-		echo $line | jq --arg url $url --arg query [$query] '{timestamp : .["@timestamp"], url : $url, query : $query, method : .method, statusCode : .statusCode, resContentLength : .req.headers."content-length", responseTime : .res.responseTime, contentLength : .res.contentLength}'
+		echo $line | jq --arg url $url --arg query [$query] '{timestamp : .["@timestamp"], url : $url, query : $query, method : .method, statusCode : .statusCode, reqContentLength : .req.headers."content-length", responseTime : .res.responseTime, contentLength : .res.contentLength}'
 	fi
 
-done < <(tail -F $input --line=+$startLine | grep response)
+done < <(tail -F $input --line=+$startLine)
